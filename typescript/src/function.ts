438cd9979650ca09d6910b89f2e3992f40e4ffa4
Axel <axelfertinel@gmail.com>
2025-11-21 10:29:02 +0100

    refactor : getDatafile

--- typescript/src/function.ts
+++ typescript/src/function.ts
@@ -2,5 +2,5 @@ import * as path from "path";
 
 export default function getDataFile(filePath: string, file: string): string {
   const base = __dirname;
-  return path.join(base, "data", "customers.csv");
+  return path.join(base, filePath, file);
 }
