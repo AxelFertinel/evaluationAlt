d14f2fb2df43b0282acfa3c290116c09fe3f686b
Axel <axelfertinel@gmail.com>
2025-11-21 11:04:57 +0100

    creation: fichier myScript avec les import typage/constants

---
+++ typescript/src/myScript.ts
@@ -0,0 +1,19 @@
+import {
+  CustomerLevel,
+  ShippingZone,
+  Currency,
+  Customer,
+  Order,
+  Product,
+  Promotion,
+} from "./type";
+
+import {
+  TAX,
+  SHIPPING_LIMIT,
+  SHIP,
+  PREMIUM_THRESHOLD,
+  LOYALTY_RATIO,
+  HANDLING_FEE,
+  MAX_DISCOUNT,
+} from "./constants";
