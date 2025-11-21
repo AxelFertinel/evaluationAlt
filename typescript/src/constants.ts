2ad808bd237cdc42a48b86312f8c7d844ed5c4ba
Axel <axelfertinel@gmail.com>
2025-11-21 11:03:57 +0100

    refacto: création d'un fichier de constants

---
+++ typescript/src/constants.ts
@@ -0,0 +1,18 @@
+// Constants shared by legacy scripts
+export const TAX = 0.2;
+export const SHIPPING_LIMIT = 50;
+export const SHIP = 5.0;
+export const PREMIUM_THRESHOLD = 1000;
+export const LOYALTY_RATIO = 0.01;
+export const HANDLING_FEE = 2.5;
+export const MAX_DISCOUNT = 200;
+
+export default {
+  TAX,
+  SHIPPING_LIMIT,
+  SHIP,
+  PREMIUM_THRESHOLD,
+  LOYALTY_RATIO,
+  HANDLING_FEE,
+  MAX_DISCOUNT,
+};
