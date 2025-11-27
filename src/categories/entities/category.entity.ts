156519f478f80f62e27c972d6666bc9590ba38dd
Axel <axelfertinel@gmail.com>
2025-11-27 11:50:51 +0100

    creation entity Category

--- src/categories/entities/category.entity.ts
+++ src/categories/entities/category.entity.ts
@@ -1,1 +1,23 @@
-export class Category {}
+import { CreateCategoryDto } from '../dto/create-category.dto';
+
+export class Category {
+  constructor(CreateCategoryDto: CreateCategoryDto) {
+    this.name = CreateCategoryDto.name;
+    this.description = CreateCategoryDto.description;
+    this.color_hex = CreateCategoryDto.color_hex;
+    this.createdAt = new Date();
+    this.updatedAt = new Date();
+  }
+
+  id: number;
+
+  name: string;
+
+  description: string;
+
+  color_hex: string;
+
+  createdAt: Date;
+
+  updatedAt: Date;
+}
