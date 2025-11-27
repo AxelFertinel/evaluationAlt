ab576082b5a8cf21380d7fdeadbae4017a4c7ff9
Axel <axelfertinel@gmail.com>
2025-11-27 11:39:08 +0100

    création de l'entity User

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,26 @@
-export class User {}
+import { CreateUserDto } from '../dto/create-user.dto';
+import { Department, Role } from '../../../generated/prisma/enums';
+
+export class User {
+  constructor(createUserDto: CreateUserDto) {
+    this.email = createUserDto.email;
+    this.departement = createUserDto.departement;
+    this.role = createUserDto.role;
+    this.createdAt = new Date();
+    this.updatedAt = new Date();
+  }
+
+  id: number;
+
+  email: string;
+
+  password: string;
+
+  departement: Department;
+
+  role: Role;
+
+  createdAt: Date;
+
+  updatedAt: Date;
+}
