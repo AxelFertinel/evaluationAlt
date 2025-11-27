c092bfe42751400e9eec192730939975aad5c0e1
Axel <axelfertinel@gmail.com>
2025-11-27 13:08:09 +0100

    création user_tool_acces entity

--- src/user_tool_access/entities/user_tool_access.entity.ts
+++ src/user_tool_access/entities/user_tool_access.entity.ts
@@ -1,1 +1,36 @@
-export class UserToolAccess {}
+import { CreateUserToolAccessDto } from '../dto/create-user_tool_access.dto';
+import { Status } from '../../../generated/prisma/enums';
+export class UserToolAccess {
+  constructor(CreateUserToolAccessDto: CreateUserToolAccessDto) {
+    this.user_id = CreateUserToolAccessDto.user_id;
+    this.tool_id = CreateUserToolAccessDto.tool_id;
+    this.granted_at = CreateUserToolAccessDto.granted_at;
+    this.granted_by = CreateUserToolAccessDto.granted_by;
+    this.revoked_at = CreateUserToolAccessDto.revoked_at;
+    this.revoked_by = CreateUserToolAccessDto.revoked_by;
+
+    status: Status;
+    this.createdAt = new Date();
+    this.updatedAt = new Date();
+  }
+
+  id?: number;
+
+  user_id: number;
+
+  tool_id: number;
+
+  granted_at: Date;
+
+  granted_by: number;
+
+  revoked_at: Date;
+
+  revoked_by: number;
+
+  status: Status;
+
+  createdAt?: Date;
+
+  updatedAt?: Date;
+}
