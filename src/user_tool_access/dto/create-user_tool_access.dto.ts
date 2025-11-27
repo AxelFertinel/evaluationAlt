2ad0d4068d588d09bc20f370869d24db6dd6c9b3
Axel <axelfertinel@gmail.com>
2025-11-27 11:35:17 +0100

    création des DTO create-user_tool_access

--- src/user_tool_access/dto/create-user_tool_access.dto.ts
+++ src/user_tool_access/dto/create-user_tool_access.dto.ts
@@ -1,1 +1,31 @@
-export class CreateUserToolAccessDto {}
+import { IsNotEmpty, IsNumber, IsDate, IsEnum } from 'class-validator';
+import { Status } from '../../../generated/prisma/enums';
+export class CreateUserToolAccessDto {
+  @IsNotEmpty()
+  @IsNumber()
+  user_id: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  tool_id: number;
+
+  @IsNotEmpty()
+  granted_at: Date;
+
+  @IsNotEmpty()
+  @IsNumber()
+  granted_by: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @IsDate()
+  revoked_at: Date;
+
+  @IsNotEmpty()
+  @IsNumber()
+  revoked_by: number;
+
+  @IsNotEmpty()
+  @IsEnum(Status)
+  status: Status;
+}
