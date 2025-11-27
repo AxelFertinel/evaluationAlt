de82f99098334c2496dc058c85dfff8041768166
Axel <axelfertinel@gmail.com>
2025-11-27 11:17:06 +0100

    création des DTO acces_requests

--- src/access_requests/dto/create-access_request.dto.ts
+++ src/access_requests/dto/create-access_request.dto.ts
@@ -1,1 +1,31 @@
-export class CreateAccessRequestDto {}
+import { IsNotEmpty, IsEnum, IsDate } from 'class-validator';
+import { RequestStatus } from '../../../generated/prisma/enums';
+
+export class CreateAccessRequestDto {
+  @IsNotEmpty()
+  user_id: number;
+
+  @IsNotEmpty()
+  tool_id: number;
+
+  @IsNotEmpty()
+  business_justification: string;
+
+  @IsNotEmpty()
+  @IsEnum(RequestStatus)
+  status: RequestStatus;
+
+  @IsDate()
+  @IsNotEmpty()
+  requested_at: Date;
+
+  @IsDate()
+  @IsNotEmpty()
+  processed_at: Date;
+
+  @IsNotEmpty()
+  processed_by: number;
+
+  @IsNotEmpty()
+  processing_notes: string;
+}
