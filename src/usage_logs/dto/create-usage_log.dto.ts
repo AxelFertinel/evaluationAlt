1b500a15a5e6d4c358a7bb58e496bef2e982d926
Axel <axelfertinel@gmail.com>
2025-11-27 11:32:34 +0100

    création DTO usage_logs

--- src/usage_logs/dto/create-usage_log.dto.ts
+++ src/usage_logs/dto/create-usage_log.dto.ts
@@ -1,1 +1,23 @@
-export class CreateUsageLogDto {}
+import { IsNotEmpty, IsDate, IsNumber } from 'class-validator';
+
+export class CreateUsageLogDto {
+  @IsNotEmpty()
+  @IsNumber()
+  user_id: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  tool_id: number;
+
+  @IsNotEmpty()
+  @IsDate()
+  session_date: Date;
+
+  @IsNotEmpty()
+  @IsNumber()
+  usage_minutes: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  actions_count: number;
+}
