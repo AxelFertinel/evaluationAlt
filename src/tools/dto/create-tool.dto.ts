2f4ee998dbbda4e194b4dabc6df06a9a0e598d9d
Axel <axelfertinel@gmail.com>
2025-11-27 11:28:56 +0100

    création DTO tools

--- src/tools/dto/create-tool.dto.ts
+++ src/tools/dto/create-tool.dto.ts
@@ -1,1 +1,35 @@
-export class CreateToolDto {}
+import { IsNotEmpty, IsEnum, IsNumber } from 'class-validator';
+import { Department, RequestStatus } from '../../../generated/prisma/enums';
+export class CreateToolDto {
+  @IsNotEmpty()
+  name: string;
+
+  @IsNotEmpty()
+  description: string;
+
+  @IsNotEmpty()
+  vendor: string;
+
+  @IsNotEmpty()
+  website_url: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  category_id: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  monthly_cost: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  active_users_count: number;
+
+  @IsNotEmpty()
+  @IsEnum(Department)
+  owner_department: Department;
+
+  @IsNotEmpty()
+  @IsEnum(RequestStatus)
+  status: RequestStatus;
+}
