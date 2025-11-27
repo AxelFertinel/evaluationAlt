8b7562442ae8ca8f0a4758f9d8ee5909bf76cb5a
Axel <axelfertinel@gmail.com>
2025-11-27 11:23:05 +0100

    création DTO create-cost_tracking

--- src/cost_tracking/dto/create-cost_tracking.dto.ts
+++ src/cost_tracking/dto/create-cost_tracking.dto.ts
@@ -1,1 +1,17 @@
-export class CreateCostTrackingDto {}
+import { IsNotEmpty, IsDate, IsDecimal } from 'class-validator';
+
+export class CreateCostTrackingDto {
+  @IsNotEmpty()
+  tool_id: number;
+
+  @IsNotEmpty()
+  @IsDate()
+  month_year: Date;
+
+  @IsNotEmpty()
+  @IsDecimal()
+  total_monthlycost: number;
+
+  @IsNotEmpty()
+  active_users_count: number;
+}
