0b2c323d390659658cb323c99d1056bc746a86ce
Axel <axelfertinel@gmail.com>
2025-11-27 11:08:57 +0100

    création des user DTO

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,26 @@
-export class CreateUserDto {}
+import { IsNotEmpty, IsEmail, IsEnum, IsDate } from 'class-validator';
+import { Role, Department } from '../../../generated/prisma/enums';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsEnum(Department)
+  departement: Department;
+
+  @IsNotEmpty()
+  @IsEnum(Role)
+  role: Role;
+
+  @IsNotEmpty()
+  status: boolean;
+
+  @IsNotEmpty()
+  @IsDate()
+  hire_date: Date;
+}
