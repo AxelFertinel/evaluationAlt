85949d6765b10de67eaf4cf40faf25200e7a0ce9
Axel <axelfertinel@gmail.com>
2025-11-27 12:02:22 +0100

    création usage_log entity

--- src/usage_logs/entities/usage_log.entity.ts
+++ src/usage_logs/entities/usage_log.entity.ts
@@ -1,1 +1,29 @@
-export class UsageLog {}
+import { CreateUsageLogDto } from '../dto/create-usage_log.dto';
+
+export class UsageLog {
+  constructor(CreateUsageLogDto: CreateUsageLogDto) {
+    this.user_id = CreateUsageLogDto.user_id;
+    this.tool_id = CreateUsageLogDto.tool_id;
+    this.session_date = CreateUsageLogDto.session_date;
+    this.usage_minutes = CreateUsageLogDto.usage_minutes;
+    this.actions_count = CreateUsageLogDto.actions_count;
+    this.createdAt = new Date();
+    this.updatedAt = new Date();
+  }
+
+  id?: number;
+
+  user_id: number;
+
+  tool_id: number;
+
+  session_date: Date;
+
+  usage_minutes: number;
+
+  actions_count: number;
+
+  createdAt?: Date;
+
+  updatedAt?: Date;
+}
