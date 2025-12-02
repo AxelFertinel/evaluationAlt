b929b6908b837b3c9aecb7354f76dfb15f82c2ed
Axel <axelfertinel@gmail.com>
2025-12-02 16:36:02 +0100

    ajout d'un hover sur les statCard

--- front/src/components/statCard.tsx
+++ front/src/components/statCard.tsx
@@ -1,7 +1,7 @@
 import type { statCard } from "@/interfaces/statCard";
 
 export const StatCard = ({ title, value, icon, badge, color }: statCard) => (
-  <div className="rounded-lg border bg-card shadow-sm flex flex-col h-full p-5">
+  <div className="rounded-lg border bg-card shadow-sm flex flex-col h-full p-5 hover:shadow-md">
     <div className="flex justify-between mb-5">
       <p>{title}</p>
       <div className={`p-1.5 ${color} rounded-lg`}>{icon}</div>
