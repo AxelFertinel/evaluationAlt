6f1206222399492d167ad22788c61947b88ae70d
Axel <axelfertinel@gmail.com>
2025-12-02 10:59:35 +0100

    ajout premier section home

--- front/src/pages/Home.tsx
+++ front/src/pages/Home.tsx
@@ -1,7 +1,57 @@
 const Home = () => {
   return (
     <>
-      <h1>Home</h1>
+      <div className="container">
+        <div className="mb-5 mt-5 border">
+          <h1>Header</h1>
+          <p>Monitoring</p>
+        </div>
+        <div className="mb-5  grid grid-cols-1 md:grid-cols-4 gap-6">
+          <div className="rounded-lg border bg-card text-card-foreground shadow-sm flex flex-col h-full p-5">
+            <div className="flex justify-between  mb-5">
+              <p>title</p>
+              <p>Icone</p>
+            </div>
+            <div>
+              <p>Price/data</p>
+              <p>12%</p>
+            </div>
+          </div>
+          <div className="rounded-lg border bg-card text-card-foreground shadow-sm flex flex-col h-full p-5">
+            <div className="flex justify-between  mb-5">
+              <p>title</p>
+              <p>Icone</p>
+            </div>
+            <div>
+              <p>Price/data</p>
+              <p>12%</p>
+            </div>
+          </div>
+          <div className="rounded-lg border bg-card text-card-foreground shadow-sm flex flex-col h-full p-5">
+            <div className="flex justify-between  mb-5">
+              <p>title</p>
+              <p>Icone</p>
+            </div>
+            <div>
+              <p>Price/data</p>
+              <p>12%</p>
+            </div>
+          </div>
+          <div className="rounded-lg border bg-card text-card-foreground shadow-sm flex flex-col h-full p-5">
+            <div className="flex justify-between  mb-5">
+              <p>title</p>
+              <p>Icone</p>
+            </div>
+            <div>
+              <p>Price/data</p>
+              <p>12%</p>
+            </div>
+          </div>
+        </div>
+        <div className=" border">
+          <p>Tableau</p>
+        </div>
+      </div>
     </>
   );
 };
