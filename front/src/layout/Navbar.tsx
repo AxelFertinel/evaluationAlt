403a59e13923b0789f434acd85cf2033119c0c25
Axel <axelfertinel@gmail.com>
2025-12-02 13:44:23 +0100

    ajout des picto et search tool

--- front/src/layout/Navbar.tsx
+++ front/src/layout/Navbar.tsx
@@ -1,5 +1,5 @@
 import { Link, useLocation } from "react-router-dom";
-import { Zap } from "lucide-react";
+import { Bell, Moon, Search, Settings, UserRoundPen, Zap } from "lucide-react";
 const Navbar = () => {
   const location = useLocation();
   const isActive = (path: string) => {
@@ -11,21 +11,9 @@ const Navbar = () => {
         <Zap />
         <span className="font-semibold text-xl tracking-tight">TechCorp</span>
       </div>
-      <div className="block lg:hidden">
-        <button className="flex items-center px-3 py-2 border rounded  hover:text-black hover:border-black">
-          <svg
-            className="fill-current h-3 w-3"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <title>Menu</title>
-            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-          </svg>
-        </button>
-      </div>
 
       <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-        <div className="text-sm lg:flex-grow flex gap-3 ">
+        <div className="text-sm mt-1 lg:flex-grow flex gap-3 ">
           <Link
             to="/"
             className={`rounded-md px-3 py-2 text-sm font-medium transition-colors ${
@@ -67,8 +55,21 @@ const Navbar = () => {
             Settings
           </Link>
         </div>
-        <div>
-          <p>Profil</p>
+        <div className="flex mt-1 gap-3">
+          <div className="border rounded-lg p-1 flex">
+            <div className="">
+              <Search size={18} color="gray" className="" />
+            </div>
+            <input
+              data-slot="input-group-control"
+              className=""
+              placeholder="Search tools..."
+            />
+          </div>
+          <Moon size={25} />
+          <Bell size={25} />
+          <Settings size={25} />
+          <UserRoundPen size={25} />
         </div>
       </div>
     </nav>
