f8cd57b807695281a6a6d5bbd17d2f6aa0bea5f0
Axel <axelfertinel@gmail.com>
2025-12-03 09:42:48 +0100

    création de la page tools

--- front/src/pages/tools/Tools.tsx
+++ front/src/pages/tools/Tools.tsx
@@ -1,7 +1,12 @@
 const Tools = () => {
   return (
     <>
-      <h1>Tools</h1>
+      <div className="container">
+        <div className="mb-5 mt-5">
+          <h1>Tools list</h1>
+          <p>Page that list all your tools information.</p>
+        </div>
+      </div>
     </>
   );
 };
