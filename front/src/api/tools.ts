c1b2e84da20f47c2b06374b1b37c16618cdd2763
Axel <axelfertinel@gmail.com>
2025-12-03 09:32:33 +0100

    ajout d'erreur au fetchtools

--- front/src/api/tools.ts
+++ front/src/api/tools.ts
@@ -7,6 +7,9 @@ export async function getTools() {
     const { data } = await api.get(`tools?_embed=user_tool`);
     return data;
   } catch (error) {
-    console.log(error);
+    throw new Error(
+      "Failed to fetch tools: " +
+        (error instanceof Error ? error.message : String(error))
+    );
   }
 }
