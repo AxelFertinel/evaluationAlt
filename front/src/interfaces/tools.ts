b76b8c15d78a8066ba179d92cd13258c197e98ba
Axel <axelfertinel@gmail.com>
2025-12-03 14:14:46 +0100

    modification type tool

--- front/src/interfaces/tools.ts
+++ front/src/interfaces/tools.ts
@@ -4,7 +4,11 @@ export type Tool = {
   category: string;
   monthly_cost: number;
   owner_department: string;
-  active_users_count: number;
+  previous_month_cost?: number;
+  website_url?: string;
+  icon_url?: string;
   status: "active" | "unused" | "expiring";
+  active_users_count: number;
+  created_at?: string;
   updated_at: string;
 };
